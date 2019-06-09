52076e2e2123ce22f450ec5a8692f5e943d23c06
hirdbluebird <bluebirdyourdestiny@gmail.com>
2019-06-09 15:48:42 +0300

    clean, working boilerplate

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import babel from 'rollup-plugin-babel'
+import cjs from 'rollup-plugin-commonjs'
+import globals from 'rollup-plugin-node-globals'
+import replace from 'rollup-plugin-replace'
+import resolve from 'rollup-plugin-node-resolve'
+
+const external = [
+  // 'antd',
+  // 'lodash',
+  // 'uuid',
+  // 'react-image',
+  '@react-pdf/renderer',
+  'react',
+  'react-dom'
+]
+
+export default {
+  input: 'src/main.js',
+  output: {
+    file: 'build/app.js',
+    format: 'iife',
+    name: 'bundle'
+  },
+
+  // Specify here external modules which you don't want to include in your bundle (for instance: 'lodash', 'moment' etc.)
+  // https://rollupjs.org/guide/en#external-e-external
+  external,
+
+  plugins: [
+    // Compile TypeScript/JavaScript files
+    babel({
+      babelrc: false,
+      exclude: 'node_modules/**',
+      presets: [ '@babel/preset-env', '@babel/preset-react' ],
+      plugins: []
+    }),
+    // Allow bundling cjs modules. Rollup doesn't understand cjs
+    cjs({
+      exclude: 'src/**'
+    }),
+    globals(),
+    replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+    resolve()
+  ]
+}
